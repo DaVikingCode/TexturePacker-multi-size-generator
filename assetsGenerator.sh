87a5efaeced2c718eb4afdaa98a2c1848dc88ac2
Aymeric <aymeric.lamboley@gmail.com>
2013-11-20 13:34:05 +0100

    optimizations

--- assetsGenerator.sh
+++ assetsGenerator.sh
@@ -6,10 +6,6 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 for i in 1 2 4
 do
 	TexturePacker --sheet "$DIR"/../bin/assets/"$i"x/assets.png  --data "$DIR"/../bin/assets/"$i"x/assets.xml  --scale $(echo "scale=2; $i/4" | bc) --format sparrow "$DIR"/assets.tps
-done
-
-for i in 1 2 4
-do
 	echo "pngquant image" "$i"x
 	pngquant -f --ext .png "$DIR"/../bin/assets/"$i"x/assets.png
 done
