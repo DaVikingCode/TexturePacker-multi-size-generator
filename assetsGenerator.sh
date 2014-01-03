4c88e29a749df91235b1deebdfb6fbcec0b70560
Aymeric <aymeric.lamboley@gmail.com>
2014-01-03 16:07:27 +0100

    TexturePacker with multipack options

--- assetsGenerator.sh
+++ assetsGenerator.sh
@@ -3,11 +3,22 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-for i in 1 1.5 2 4
+resolutions=(1 1.5 2 4)
+
+for i in "${resolutions[@]}"
 do
-	TexturePacker --sheet "$DIR"/../bin/assets/"$i"x/assets.png  --data "$DIR"/../bin/assets/"$i"x/assets.xml  --scale $(echo "scale=3; $i/4" | bc) --format sparrow "$DIR"/assets.tps
-	echo "pngquant image" "$i"x
-	pngquant -f --ext .png "$DIR"/../bin/assets/"$i"x/assets.png
+	
+	if [ $i = 1.5 ]
+	then
+		maxSize=$(echo "2*1024" | bc)
+	else
+		maxSize=$(echo "$i*1024" | bc)
+	fi
+	
+	TexturePacker --sheet "$DIR"/../bin/assets/"$i"x/assets{n}.png  --data "$DIR"/../bin/assets/"$i"x/assets{n}.xml  --scale $(echo "scale=3; $i/4" | bc) --max-size "$maxSize" --multipack --format sparrow "$DIR"/assets.tps
+	echo "pngquant image(s)" "$i"x
+	pngquant -f --ext .png "$DIR"/../bin/assets/"$i"x/*.png
+	
 done
 
 echo "TexturePacker and pngquant tasks done."
